39a19f246d763f54902fe94b3f5f0a6dcc9d19d5
Vaxood <110937853+Vaxood@users.noreply.github.com>
2023-01-22 19:48:24 +0100

    Update Program.cs

--- auto/Program.cs
+++ auto/Program.cs
@@ -1,4 +1,3 @@
-﻿
 using System.Text.RegularExpressions;
 Console.Write("Path?:");
 string path = Console.ReadLine();
@@ -6,14 +5,14 @@ string alltext = File.ReadAllText(path);
 List<string> myList = new List<string>();
 int k = 0;
 string textfinial =""; 
-foreach (Match match in Regex.Matches(alltext, @"Color\((?<r>\d{1,3}),(?<g>\d{1,3}),(?<b>\d{1,3})\)"))
+foreach (Match match in Regex.Matches(alltext, @"Color\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*(,\s*(\d{1,3}))?\s*\)"))
 {
     alltext = alltext.Replace(match.Value,"color"+k);
     textfinial = textfinial + "local color"+k+" = "+match.Value+"\n";
     k++;
 }
 textfinial = textfinial +"\n\n"+alltext;
+Console.Write("Done ! u can close this window now!");
 
-Console.Write("done");
 File.WriteAllText(path,textfinial);
 Console.ReadLine();
